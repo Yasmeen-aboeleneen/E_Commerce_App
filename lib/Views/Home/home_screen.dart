45119df2cb3d0ce235f943d5f004b0d518742763
Yasmeen-aboeleneen <yasmeenaboeleneen3@gmail.com>
2024-08-14 02:23:36 +0300

    BottomNavigationBar

--- lib/Views/Home/home_screen.dart
+++ lib/Views/Home/home_screen.dart
@@ -1,10 +1,66 @@
+import 'package:ecommerce/Core/Constants/colors.dart';
+import 'package:ecommerce/Views/Cart/cart_screen.dart';
+import 'package:ecommerce/Views/Category/categories_screen.dart';
+import 'package:ecommerce/Views/Home/home_screen_body.dart';
+import 'package:ecommerce/Views/Settings/settings_screen.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = const [
+    HomeScreenBody(),
+    CategoriesScreen(),
+    CartScreen(),
+    SettingsScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        backgroundColor: kveryWhite,
+        elevation: 0,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard_outlined),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: kPrimary,
+        unselectedItemColor:
+            Colors.grey, // Optional: Customize unselected item color
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
